2939968fd92d4eca0a7859eca8c7c856d82dbbc8
Eyoatam Tamirat <eyoatamtamirat7@gmail.com>
2021-04-07 21:16:18 +0300

    update indentation in doc comments

--- types/progress.d.ts
+++ types/progress.d.ts
@@ -19,17 +19,17 @@ interface BarOptions {
  * ```ts
  * import { Progressbar } from "../src/progress.ts";
  *
- *const bar = new Progressbar("  :title |:bar| eta: :eta :percent", {
+ * const bar = new Progressbar("  :title |:bar| eta: :eta :percent", {
  *  total: 100,
- *});
+ * });
  *
- *const id = setInterval(() => {
+ * const id = setInterval(() => {
  *  bar.tick(1, { title: "progress " });
  *
  *  if (bar.complete === true) {
  *    clearInterval(id);
  *  }
- *}, 50);
+ * }, 50);
  *
  * ```
  *
